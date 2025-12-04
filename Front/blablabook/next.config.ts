0c81f2a5ccef32644a52cee4895d6724d7444037
samuel <samuel@webilo.fr>
2025-12-04 16:32:19 +0100

    FIX dockerfile

--- Front/blablabook/next.config.ts
+++ Front/blablabook/next.config.ts
@@ -12,7 +12,6 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   webpack: (config, context) => {
-    // Enable polling based on env variable being set
     if(process.env.NEXT_WEBPACK_USEPOLLING) {
       config.watchOptions = {
         poll: 200,
