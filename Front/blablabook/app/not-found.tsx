a982e001338cc1efe11f2a60a07d68945c1e98cb
PatignierThomas <thomas.patignier@gmail.com>
2025-12-04 16:48:10 +0100

    docker path cleaning

---
+++ Front/blablabook/app/not-found.tsx
@@ -0,0 +1,12 @@
+export default function NotFound() {
+  return (
+    <>
+      <div className="wrapper">
+        <p className="font-one bg-quater">404 - Page Not Found</p>
+        <p className="text-primary bg-tertiary p-2 font-bold font-one">
+          Oops! The page you are looking for does not exist.
+        </p>
+      </div>
+    </>
+  );
+}
