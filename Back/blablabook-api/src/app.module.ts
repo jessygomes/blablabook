25fe3365bef9925e69b7ee95106807e5bc8c388f
PatignierThomas <thomas.patignier@gmail.com>
2025-12-05 18:39:05 +0100

    fixing merge error

--- Back/blablabook-api/src/app.module.ts
+++ Back/blablabook-api/src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { BooksModule } from './books/books.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UsersModule, BookModule, ConfigModule.forRoot()],
+  imports: [AuthModule, UsersModule, BooksModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
