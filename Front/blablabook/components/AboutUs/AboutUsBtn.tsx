ce283ba401886e899514603c65de42c2de9fcaef
Jessy <you@example.com>
2026-01-08 14:13:52 +0100

    fix about

--- Front/blablabook/components/AboutUs/AboutUsBtn.tsx
+++ Front/blablabook/components/AboutUs/AboutUsBtn.tsx
@@ -5,10 +5,9 @@ export default function AboutUsBtn() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <div>
-      {" "}
       <button
         onClick={() => setIsModalOpen(true)}
-        className="text-white cursor-pointer bg-linear-to-r from-primary to-secondary hover:bg-primary tracking-wider rounded-[5px] text-base font-one font-semibold p-2.5 drop-shadow-md"
+        className="text-white cursor-pointer bg-primary hover:bg-primary/80 tracking-wider rounded-[5px] text-base font-one font-semibold p-2.5 drop-shadow-md"
       >
         En savoir plus
       </button>
