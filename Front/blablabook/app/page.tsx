d70ce83b1981e39aeecc0069ca1c32f949052d0d
David BASSI <david.bassi.db@gmail.com>
2026-01-08 12:08:17 +0100

    fix display header

--- Front/blablabook/app/page.tsx
+++ Front/blablabook/app/page.tsx
@@ -1,8 +1,10 @@
+import Header from "@/components/Header";
 import BookLists from "@/components/Home/BookLists";
 
 export default function Home() {
   return (
     <>
+      <Header/>
       <div className="wrapper">
         <BookLists />
       </div>
