e09e6633f0a5933b1c3f6c685fb6d0cfb2c8c73e
David BASSI <david.bassi.db@gmail.com>
2026-01-27 09:32:38 +0100

    Retrieve the total count of comments

--- Back/blablabook-api/src/comment/comment.controller.ts
+++ Back/blablabook-api/src/comment/comment.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Query } from '@nestjs/common';
 import { CommentService } from './comment.service';
 
 @Controller('comments')
 export class CommentController {
   constructor(private service: CommentService) {}
+
+  @Get('comment-count')
+  async getCommentCount() {
+    const commentCount = await this.service.getCommentCount();
+    if (commentCount > 0) {
+      return commentCount;
+    } else {
+      throw new NotFoundException('Aucun commentaire trouvé');
+    }
+  }
 
   @Get('latest-per-book')
   latestPerBook(@Query('take') take?: string) {
